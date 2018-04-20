d7756b54c787b5679232cc032ec822101d849f8b
Md. Asadul Islam <asad.cse.ru.15@gmail.com>
2018-04-20 18:35:13 +0600

    Edited Solution for 136

--- 136-Single Number.cpp
+++ 136-Single Number.cpp
@@ -4,6 +4,23 @@
  */
 
 
+/**
+ * Another solution:
+ * The trick is: A^B^A=B
+ * class Solution {
+	public:
+    	int singleNumber(vector<int>& nums) {
+        	if(nums.empty()) return 0;
+        	int first = nums[0];
+        	for(int i = 1; i<nums.size();i++) {
+            	first = first ^ nums[i];
+        	}
+        	return first;
+    	}
+	};
+ */
+
+
 #include "bits/stdc++.h"
 using namespace std;
 
